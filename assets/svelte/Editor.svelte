6b14f054f0b8a35cbf90c876724156e98915306d
Kyle Cierzan <kcierzan@gmail.com>
2023-10-10 13:12:01 -0400

    Refactor editor component

--- assets/svelte/Editor.svelte
+++ assets/svelte/Editor.svelte
@@ -1,24 +1,64 @@
 <script lang="ts">
   import { afterUpdate } from "svelte";
   import { flash } from "js/utils";
-  import { get } from "svelte/store"
+  import { get } from "svelte/store";
   import selectedStore from "js/stores/selected";
   import type { SelectedStore } from "js/stores/selected";
   import trackDataStore from "js/stores/track-data";
   import playerStore from "js/stores/players";
   import WaveSurfer from "wavesurfer.js";
   import Regions from "wavesurfer.js/dist/plugins/regions.js";
-  import type { RegionParams } from "wavesurfer.js/dist/plugins/regions.js";
+  import type {
+    Region,
+    RegionParams,
+  } from "wavesurfer.js/dist/plugins/regions.js";
   import type { Clip, TrackData } from "js/types";
   import clipMessage from "js/clip";
 
   let waveformContainer: HTMLElement;
   let waveform: any;
-  let region: any;
 
   afterUpdate(() => {
     flash(waveformContainer);
   });
+
+  $: selectedClip = getSelectedClip($selectedStore, $trackDataStore);
+  $: drawWaveform(selectedClip);
+
+  function drawWaveform(currentClip?: Clip) {
+    !!waveform && waveform.destroy();
+    if (!!currentClip?.audio_file) {
+      waveform = WaveSurfer.create({
+        container: waveformContainer,
+        waveColor: "#06b6d4",
+        interact: false,
+        cursorWidth: 0,
+        url: currentClip.audio_file.file.url,
+      });
+      waveform.on("decode", () => createPlaybackRegion(currentClip, waveform));
+    }
+  }
+
+  function createPlaybackRegion(currentClip: Clip, waveform: WaveSurfer) {
+    const clipDuration =
+      get(playerStore)[currentClip.id].grainPlayer!.buffer.duration;
+    const regions = waveform.registerPlugin(Regions.create());
+    const regionParams: RegionParams = {
+      start: currentClip.start_time,
+      end: currentClip.end_time ?? clipDuration,
+      color: "rgba(34, 211, 238, 0.5)",
+      drag: true,
+      resize: true,
+    };
+    regions.addRegion(regionParams);
+    regions.on("region-updated", (region: Region) => {
+      clipMessage.push.updateClips({
+        ...currentClip,
+        start_time: region.start,
+        end_time: region.end,
+      });
+    });
+  }
 
   function getSelectedClip(selected: SelectedStore, trackData: TrackData[]) {
     if (selected.clipId && selected.trackId) {
@@ -27,46 +67,6 @@
         ?.audio_clips.find((clip: Clip) => clip.id === selected.clipId);
     }
   }
-
-  function drawWaveform(currentClip?: Clip) {
-    if (!!waveform) waveform.destroy();
-
-    if (!!currentClip?.audio_file) {
-      const clipDuration = get(playerStore)[currentClip.id].grainPlayer!.buffer.duration
-      waveform = WaveSurfer.create({
-        container: waveformContainer,
-        waveColor: "#06b6d4",
-        progressColor: "#06b6d4",
-        interact: false,
-        cursorWidth: 0,
-        url: currentClip.audio_file.file.url,
-      });
-      region = waveform.registerPlugin(Regions.create());
-      waveform.on("decode", () => {
-        const regionParams: RegionParams = {
-          start: currentClip.start_time,
-          end: clipDuration,
-          color: "rgba(34, 211, 238, 0.5)",
-          drag: true,
-          resize: true,
-        };
-        if (currentClip.end_time) {
-          regionParams.end = currentClip.end_time;
-        }
-        region.addRegion(regionParams);
-      });
-      region.on("region-updated", (region: any) => {
-        clipMessage.push.updateClips({
-          ...currentClip,
-          start_time: region.start,
-          end_time: region.end,
-        });
-      });
-    }
-  }
-
-  $: selectedClip = getSelectedClip($selectedStore, $trackDataStore);
-  $: drawWaveform(selectedClip);
 </script>
 
 <section class="p-4">
