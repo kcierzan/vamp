ba50c646585630cd8655aec5b45cfd116e794515
Kyle Cierzan <kcierzan@gmail.com>
2023-11-18 08:51:35 -0500

    Add specs for files channel

--- lib/vamp_web/channels/projects/files_channel.ex
+++ lib/vamp_web/channels/projects/files_channel.ex
@@ -10,12 +10,23 @@ defmodule VampWeb.FilesChannel do
     validate_topic_matches_socket(song_id, socket, __MODULE__)
   end
 
+  @spec handle_in(
+          binary()
+          | maybe_improper_list(
+              binary() | maybe_improper_list(any(), binary() | []) | byte(),
+              binary() | []
+            ),
+          {:binary, any()},
+          any()
+        ) :: {:noreply, any()}
   def handle_in(audio_file_json, {:binary, data}, socket) do
-    audio_file_json
-    |> Jason.decode!()
+    audio_file_attrs = Jason.decode!(audio_file_json)
+    %{"song_id" => song_id} = audio_file_attrs
+
+    audio_file_attrs
     |> add_waffle_file_attr(data)
     |> Vamp.Sounds.create_pool_audio_file()
-    |> broadcast_new_pool_item!()
+    |> broadcast_new_pool_item!(song_id)
 
     {:noreply, socket}
   end
@@ -25,10 +36,10 @@ defmodule VampWeb.FilesChannel do
     put_in(attrs["file"], %{filename: URI.encode(attrs["name"]), binary: data})
   end
 
-  defp broadcast_new_pool_item!(audio_file) do
+  defp broadcast_new_pool_item!(audio_file, song_id) do
     Endpoint.broadcast_from(
       self(),
-      "song_data:" <> audio_file.song_id,
+      "song_data:" <> song_id,
       "new_pool_file",
       audio_file
     )
---
+++ test/vamp_web/channels/projects/files_channel_test.exs
@@ -0,0 +1,80 @@
+defmodule VampWeb.Projects.FilesChannelTest do
+  use VampWeb.ChannelCase, async: true
+
+  describe "join" do
+    setup do
+      user = Vamp.AccountsFixtures.user_fixture()
+      song = Vamp.ProjectsFixtures.song_fixture(%{created_by_id: user.id})
+
+      %{user: user, song: song}
+    end
+
+    test "join is successful when the topic matches the socket song", %{user: user, song: song} do
+      assert {:ok, _, _socket} =
+               VampWeb.UserSocket
+               |> socket("user_id", %{current_user: user, song_id: song.id})
+               |> subscribe_and_join(
+                 VampWeb.FilesChannel,
+                 "song_files:" <> song.id
+               )
+    end
+
+    test "join fails on socket song topic song mismatch", %{user: user, song: song} do
+      assert {:error, %{"reason" => "unauthorized"}} =
+               VampWeb.UserSocket
+               |> socket("user_id", %{current_user: user, song_id: song.id})
+               |> subscribe_and_join(
+                 VampWeb.FilesChannel,
+                 "song_files:foobar"
+               )
+    end
+  end
+
+  describe "handle_in audio file JSON" do
+    setup do
+      user = Vamp.AccountsFixtures.user_fixture()
+      song = Vamp.ProjectsFixtures.song_fixture(%{created_by_id: user.id})
+
+      {:ok, _, socket} =
+        VampWeb.UserSocket
+        |> socket("user_id", %{current_user: user, song_id: song.id})
+        |> subscribe_and_join(
+          VampWeb.FilesChannel,
+          "song_files:" <> song.id
+        )
+
+      {:ok}
+
+      %{user: user, song: song, socket: socket}
+    end
+
+    test "JSON is parsed, an audio file is created, and a pool item is broadcast", %{
+      song: song,
+      socket: socket
+    } do
+      song_data_topic = "song_data:" <> song.id
+      VampWeb.Endpoint.subscribe(song_data_topic)
+
+      {:ok, audio_bytes} = File.read("test/support/fixtures/samples/100action.wav")
+
+      file_attrs = %{
+        "media_type" => "audio/wav",
+        "size" => 100,
+        "name" => "action.wav",
+        "bpm" => 100.0,
+        "description" => "test file",
+        "song_id" => song.id
+      }
+
+      push(socket, Jason.encode!(file_attrs), {:binary, audio_bytes})
+
+      assert_receive %Phoenix.Socket.Broadcast{
+        topic: ^song_data_topic,
+        event: "new_pool_file",
+        payload: %Vamp.Sounds.AudioFile{}
+      }
+
+      VampWeb.Endpoint.unsubscribe(song_data_topic)
+    end
+  end
+end
